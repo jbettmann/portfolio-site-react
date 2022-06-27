12af60109c9f4b4dd4bef267fd043e3ea6466e6a
jbettmann <jordan.bettmann@gmail.com>
2022-06-27 13:40:06 -0600

    added resume download icon

---
+++ src/icons/download.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { ReactComponent as DOWNLOAD } from "../img/skill_icons/download.svg";
+
+export const Download = () => {
+  return <DOWNLOAD title="download" />;
+};
