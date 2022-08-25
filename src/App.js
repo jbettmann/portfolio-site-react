e2a92374bc8827b57afffff842f6b8fc594be283
jbettmann <jordan.bettmann@gmail.com>
2022-08-25 13:49:20 -0600

    added secret passwords dracarys and fire

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import "./App.scss";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import { MainView } from "./components/main-view/main-view";
 import { BrowserRouter } from "react-router-dom";
 
 function App() {
+  const pressed = [];
+  const secretCode = "dracarys";
+  const secretCodeWest = "fire";
+
+  const [fire, setFire] = useState(false);
+
+  function handleSecret(e) {
+    console.log(e.key);
+    pressed.push(e.key);
+    pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
+    if (pressed.join("").includes(secretCode)) {
+      const fireOn = setTimeout(() => setFire(true), 0);
+      const fireOff = setTimeout(() => setFire(false), 4000);
+      console.log("ON");
+    }
+    if (pressed.join("").includes(secretCodeWest)) {
+      window.location.href =
+        "https://awoiaf.westeros.org/index.php/Dragonflame";
+    }
+    console.log(pressed);
+  }
+
+  useEffect(() => {
+    window.addEventListener("keyup", handleSecret);
+  });
+
   return (
-    <div className="App">
+    <div className={fire ? "App fire" : "App"}>
       <BrowserRouter>
         <MainView />
       </BrowserRouter>
