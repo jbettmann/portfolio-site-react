eeda0a55b1267a9c7a158501259cf5dfebd1b1d4
jbettmann <jordan.bettmann@gmail.com>
2022-08-25 13:51:53 -0600

    added secret password

--- src/App.js
+++ src/App.js
@@ -12,19 +12,16 @@ function App() {
   const [fire, setFire] = useState(false);
 
   function handleSecret(e) {
-    console.log(e.key);
     pressed.push(e.key);
     pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length);
     if (pressed.join("").includes(secretCode)) {
       const fireOn = setTimeout(() => setFire(true), 0);
       const fireOff = setTimeout(() => setFire(false), 4000);
-      console.log("ON");
     }
     if (pressed.join("").includes(secretCodeWest)) {
       window.location.href =
         "https://awoiaf.westeros.org/index.php/Dragonflame";
     }
-    console.log(pressed);
   }
 
   useEffect(() => {
